86fa4b37c428ddeac35f6b9509af879e70494684
Madhusudhan Vaddiboyna <madhusudhan.vaddiboyna@aveva.com>
2016-08-25 10:07:11 +0530

    Redbus automation repository

---
+++ src/main/java/com/redbus/objectRepository/RedBusHomePageRepo.java
@@ -0,0 +1,14 @@
+package com.redbus.objectRepository;
+
+import org.openqa.selenium.By;
+
+public class RedBusHomePageRepo 
+{	
+	public static final By txt_from_place      = By.id("txtSource");
+	public static final By txt_to_place        = By.id("txtDestination");
+	public static final By txt_date_of_journey = By.id("txtOnwardCalendar");
+	public static final By txt_date_of_return  = By.id("txtReturnCalendar");
+	public static final By btn_bus_search      = By.id("searchBtn");
+
+		
+}
